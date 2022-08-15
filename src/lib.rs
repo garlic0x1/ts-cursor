9d01dfdba68c32198142effaed3b8e51bd86f42d
garlic0x1 <garlic1@protonmail.com>
2022-08-14 22:14:23 -0700

    bugfixes and tests

--- src/lib.rs
+++ src/lib.rs
@@ -33,8 +33,9 @@ mod tests {
         let mut cursor = Cursor::from_file(&file, STKind::Concrete);
 
         assert_eq!(cursor.goto_first_child(), true);
-        eprintln!("{}", cursor.kind());
-        assert_eq!(cursor.goto_next_sibling(), true);
-        assert_eq!(cursor.name(false), Some("Data".into()));
+        assert_eq!(cursor.goto_first_child(), true);
+        assert_eq!(cursor.goto_first_child(), true);
+        assert_eq!(cursor.goto_first_child(), true);
+        assert_eq!(cursor.kind(), "<");
     }
 }
